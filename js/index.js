f40324438655e085378bbc23c74fc5b36417759c
NazarMatkovskyi <matkovskuyn@gmail.com>
2021-07-01 15:59:55 +0300

    result project

--- js/index.js
+++ js/index.js
@@ -1,44 +1,26 @@
-
-function updateClock(){
-const clockContainer = document.querySelector(".clock");
-clockContainer.innerText = new Date().toLocaleTimeString('uk');
+function updateClock() {
+  const clockContainer = document.querySelector(".clock");
+  clockContainer.innerText = new Date().toLocaleTimeString("uk");
 }
 setInterval(updateClock, 1000);
-
-
 
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("closes")[0];
 const form = document.getElementById("my_form");
 
-// btn.onclick = function() {
-// modal.style.display = "block";
-// }
-  //   form.onsubmit = function(event) {
-  //   event.preventDefault();
- 
-  // }
-  //  modal.style.display = "block";
-   form.addEventListener('submit', event => {
-     event.preventDefault();
-     // actual logic, e.g. validate the form
-    modal.style.display = "block";
-   });
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  modal.style.display = "block";
+});
 
-
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    
   }
- 
-}
-
-
-
+};
 
